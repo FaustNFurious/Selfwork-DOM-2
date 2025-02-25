97832058dbfe99a2bfdde3cd840b25bf687c4f1d
FaustNFurious <lorelego2003@gmail.com>
2025-02-25 21:41:07 +0100

    Add files via upload
    Corretto esercizio aggiungendo la data e stile all'articolo

--- Selfwork DOM 2/Main.js
+++ Selfwork DOM 2/Main.js
@@ -6,7 +6,20 @@ let button = document.querySelector('button');
 let articolo = document.querySelector('.articolo');
 
 button.addEventListener('click', () => {
-    let section = document.createElement('section');
-    section.innerHTML = `<h3>${input.value}</h3> <p>${textarea.value}</p>`;
-    articolo.appendChild(section);
+    let date = new Date();
+    let formatDate = date.toLocaleDateString();
+
+    if (input.value == '' && textarea.value == '') {
+        alert (`Inserire almeno un campo per creare l'articolo`);
+    }
+    else {
+        let section = document.createElement('section');
+        section.style.backgroundColor = 'lightblue';
+        section.style.width = '30%';
+        section.innerHTML = `<h3>${input.value}</h3> <p>${textarea.value}</p> <i>${formatDate}</i>`;
+        articolo.appendChild(section);
+        input.value = '';
+        textarea.value = '';
+    }
+
 });
